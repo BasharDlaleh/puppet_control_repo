e257af3852c786dab8aef87c5f107dbdf88b2b2e
BasharDlaleh <66019475+BasharDlaleh@users.noreply.github.com>
2023-10-28 12:27:25 +0300

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,3 +1,4 @@
+# In most programming languages a "class" is an object that can be used in complex ways. Puppet classes are more like functions or methods in other languages, a way of easily calling another set of code.
 class profile::base {
   user {'admin':
     ensure => present,
