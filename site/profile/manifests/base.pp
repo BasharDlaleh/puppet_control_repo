1600c5df6b36030d9cd65c2c3e4ac483c7e45c40
BasharDlaleh <66019475+BasharDlaleh@users.noreply.github.com>
2023-10-28 12:31:36 +0300

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,4 +1,6 @@
 # In most programming languages a "class" is an object that can be used in complex ways. Puppet classes are more like functions or methods in other languages, a way of easily calling another set of code.
+# profile are a group of modules/resources that make up one thing like all the necessary resources (user, file, package, service) to create an NGINX web server
+
 class profile::base {
   user {'admin':
     ensure => present,
