626719f1a437555c4cc33a7a833ab6d2951d0035
BasharDlaleh <66019475+BasharDlaleh@users.noreply.github.com>
2023-10-28 12:32:42 +0300

    Update app_server.pp

--- site/role/manifests/app_server.pp
+++ site/role/manifests/app_server.pp
@@ -1,3 +1,7 @@
+# roles are a group of profiles that define a role for the node machine, like a role that includes NGINX profile and MYSQL profile is a role for a dev server
+# always use unique roles for nodes even if 2 nodes has the exact same role, A major point of the roles and profiles pattern is clarity. If you introduce ambiguity in your code, you lose that benefit. 
+# Creating multiple roles for similar machines allows you to quickly and easily identify what each machine does.
+
 class role::app_server {
   include profile::base
   include profile::web
