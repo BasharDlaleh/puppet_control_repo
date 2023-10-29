a1cb578fe0d06f0d3f3a470c4c3024f5657c8fb7
BasharDlaleh <66019475+BasharDlaleh@users.noreply.github.com>
2023-10-29 15:02:36 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,5 +1,47 @@
-class minecraft {
+class minecraft (
+  # the variables we put here are exposed to the outside so we can override them when we use the class
+  $url = 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar'
+  $install_dir = '/opt/minecraft'
+)
+{
 
+  file {$install_dir:
+    ensure => directory,
+  }
+
+  file {"${install_dir}/minecraft_server.jar":
+    ensure => file,
+    source => $url,
+    before => Service['minecraft'],
+  }
+
+  package {'default-jre':
+    ensure => present,
+  }
+
+  file {"${install_dir}/eula.txt":
+    ensure  => file,
+    content => "eula=true",
+  }
+
+  file {'/etc/systemd/system/minecraft.service':
+    ensure  => file,
+    # here the file content is fetched from the templates folder using the epp built-in function in puppet, the templates folder is inside the module folder and it's useful for customizing values inside static files
+    content => epp('minecraft/minecraft.service.epp', {
+      install_dir => $install_dir,
+    }),
+  }
+
+  service {'minecraft':
+    ensure  => running,
+    enable => true,
+    require => [ Package['default-jre'], File["${install_dir}/eula.txt"], File['/etc/systemd/system/minecraft.service'] ]
+  }
+}
+
+/*
+class minecraft {
+  # here the variables are local to the class so we can not overrite them
   $url = 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar'
   $install_dir = '/opt/minecraft'
 
@@ -24,6 +66,7 @@ class minecraft {
 
   file {'/etc/systemd/system/minecraft.service':
     ensure  => file,
+    # below the file is fetched from the files folder in the module folder
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
 
@@ -33,3 +76,4 @@ class minecraft {
     require => [ Package['default-jre'], File["${install_dir}/eula.txt"], File['/etc/systemd/system/minecraft.service'] ]
   }
 }
+*/
