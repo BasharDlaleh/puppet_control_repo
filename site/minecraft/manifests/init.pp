2f01253a7bfed222f16313c253d7e01cbf21f7be
BasharDlaleh <66019475+BasharDlaleh@users.noreply.github.com>
2023-10-29 13:44:17 +0300

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,35 @@
+class minecraft {
+
+  $source = 'https://piston-data.mojang.com/v1/objects/5b868151bd02b41319f54c8d4061b8cae84e665c/server.jar'
+  $install_dir = '/opt/minecraft'
+
+  file {$install_dir:
+    ensure => directory,
+  }
+
+  file {"${install_dir}/minecraft_server.jar":
+    ensure => file,
+    source => $source,
+    before => Service['minecraft'],
+  }
+
+  package {'default-jre':
+    ensure => present,
+  }
+
+  file {"${install_dir}/eula.txt":
+    ensure  => file,
+    content => "eula=true",
+  }
+
+  file {'/etc/systemd/system/minecraft.service'
+    ensure  => file,
+    content => 'puppet:///modules/minecraft/minecraft.service',
+  }
+
+  service {'minecraft'
+    ensure  => running,
+    enabled => true,
+    require => [ Package['default-jre'], File['${install_dir}/eula.txt'], File['/etc/systemd/system/minecraft.service'] ]
+  }
+}
