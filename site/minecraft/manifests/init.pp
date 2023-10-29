d45747345a84df191301327afaae28420e89db77
BasharDlaleh <66019475+BasharDlaleh@users.noreply.github.com>
2023-10-29 14:04:08 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -30,6 +30,6 @@ class minecraft {
   service {'minecraft':
     ensure  => running,
     enable => true,
-    require => [ Package['default-jre'], File['${install_dir}/eula.txt'], File['/etc/systemd/system/minecraft.service'] ]
+    require => [ Package['default-jre'], File["${install_dir}/eula.txt"], File['/etc/systemd/system/minecraft.service'] ]
   }
 }
