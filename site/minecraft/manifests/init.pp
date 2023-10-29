cec5025d6812527b1d574cb48bd45b45212e1b2a
BasharDlaleh <66019475+BasharDlaleh@users.noreply.github.com>
2023-10-29 14:41:04 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,6 @@
 class minecraft {
 
-  $url = 'https://piston-data.mojang.com/v1/objects/5b868151bd02b41319f54c8d4061b8cae84e665c/server.jar'
+  $url = 'https://launcher.mojang.com/mc/game/1.12.2/server/886945bfb2b978778c3a0288fd7fab09d315b25f/server.jar'
   $install_dir = '/opt/minecraft'
 
   file {$install_dir:
